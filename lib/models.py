b6b7822f3b1963b81029cde079075270278f77d8
xwbxxx <49603007+xwbxxx@users.noreply.github.com>
2023-07-17 14:50:35 +0800

    Update models.py

--- lib/models.py
+++ lib/models.py
@@ -435,15 +435,15 @@ class VAEforMNIST(nn.Module):
         h1 = F.relu(self.fc1(x))
         return self.fc21(h1), self.fc22(h1)
 
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
+
     @staticmethod
     def reparameterize(mu, logvar):
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
         return mu + eps * std
-
-    def decode(self, z):
-        h3 = F.relu(self.fc3(z))
-        return torch.sigmoid(self.fc4(h3))
 
     def forward(self, x):
         mu, logvar = self.encode(x.view(-1, 784))
